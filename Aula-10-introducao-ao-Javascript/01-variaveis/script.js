68a445005186ddbce5c31213b6c0197f918f2312
DSM1 <fatec-dsm1@academico.intra>
2026-05-21 19:56:41 -0300

    Aula 10

--- Aula-10-introducao-ao-Javascript/01-variaveis/script.js
+++ Aula-10-introducao-ao-Javascript/01-variaveis/script.js
@@ -20,3 +20,27 @@ cidade = "sete barras";
 //CONST
 
 const user = "pedro@ElementInternals.com"
+
+// FUNÇÕES DO ELEMENTO WINDOW (navegador)
+// Disparando uma janela de alerta no site
+window.alert("Bem-vindo ao mundo de Javascript!")
+// Disparando uma janela de confirmação
+const confirmacao = window.confirm("Vamos começar?")
+if (confirmacao == true) {
+    //Dispara uma janela de entrada de dados
+    const nome = window.prompt("Qual é o seu nome?")
+    const idade = window.prompt("Quantos anos você têm?")
+
+
+    // Escrevendo no site
+    document.write("Olá " + nome + "!");     // Concatenação: string + variável
+    // Concatenação - Template Strings
+    // document.write("Você têm " + idade + " anos")
+    document.writeln(` Você tem ${idade} anos!`)
+
+    if (idade >= 18) {
+        document.write("Você é maior de idade!")
+    } else {
+        document.write("Você é menor de idade!")
+    }
+}
