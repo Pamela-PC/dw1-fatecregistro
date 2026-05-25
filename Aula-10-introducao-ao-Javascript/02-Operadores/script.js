4488c167a5327c66651b043f2200f9d63bd2f41b
DSM1 <fatec-dsm1@academico.intra>
2026-05-25 19:34:52 -0300

    aula 10.2

---
+++ Aula-10-introducao-ao-Javascript/02-Operadores/script.js
@@ -0,0 +1,31 @@
+let n1 = Number(window.prompt('Digite um número:'));
+let n2 = Number(window.prompt('Digite outro número:'));
+
+// Operadores 
+// Soma
+const soma = n1 + n2
+window.alert(`soma dos valores é ${soma}`)
+// Multiplicação
+let mult = n1*n2
+document.writeln(`<br>A multiplicação de ${n1} e ${n2} é igual a ${mult}`)
+// Divisão
+let  div = n1/n2
+document.writeln(`<br>A divisão de ${n1} e ${n2} é igual a ${div}`)
+// Resto da divisão
+let rest = n1%n2
+document.writeln(`<br>O resto da divisão de ${n1} e ${n2} é igual a ${rest}`)
+// Subtração
+let sub = n1-n2
+document.writeln(`<br>A subtração de ${n1} e ${n2} é igual a ${mult}`)
+// Potenciaçã
+let  pot = n1**n2
+document.writeln(`<br>A potencia de ${n1} elevado ${n2} é igual a ${mult}`)
+
+
+// Operadores de Atribuição
+n1+=n2
+document.writeln(`<br> ${n1}`)
+n1+=n2
+document.writeln(`<br> ${n1}`)
+n1+=n2
+document.writeln(`<br> ${n1}`)
